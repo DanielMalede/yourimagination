9227d79c0b961123d49b102f2961a26419c9f9c5
DanielMalede <danielmalede@gmail.com>
2023-05-08 13:36:12 +0300

    changes in create

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -14,17 +14,39 @@ export default function CreatePost() {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = () => {};
+
+  const handleChange = (e) => {};
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">
-          Create
-        </h1>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[18px] max-w[500px]">
-          Create  imagination and visually stunning
-          images through by DALL-E AI and share them
+          Create imagination and visually stunning images through by DALL-E AI
+          and share them
         </p>
       </div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            LabelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="SOME NAME"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            LabelName="prompt"
+            type="text"
+            name="prompt"
+            placeholder="SOME NAME"
+            value={form.name}
+            handleChange={handleChange}
+          />
+        </div>
+      </form>
     </section>
   );
 }
