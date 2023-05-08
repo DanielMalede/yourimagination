e9b2eda79fb55720c40f3fa264a606f97be8b80e
DanielMalede <danielmalede@gmail.com>
2023-05-08 18:53:52 +0300

    server set up

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,30 +1,44 @@
-import React from 'react'
+import React from "react";
 
-export default function FormField({labelName,name , type ,value,placeholder,handleChange, isSurpriseMe, handleSurpriseMe}) {
+export default function FormField({
+  labelName,
+  name,
+  type,
+  value,
+  placeholder,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) {
   return (
     <div>
-      <div className='flex items-center gap-2 mb-2'>
+      <div className="flex items-center gap-2 mb-2">
         <label
-        htmlFor={name}
-        className='block text-sm font-medium text-gray-900'>
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
           {labelName}
         </label>
         {isSurpriseMe && (
           <button
-          type='button'
-          onClick={handleSurpriseMe}
-          className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded[5px] text-black'>
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded[5px] text-black"
+          >
             Surprise Me
-          </button>)}
+          </button>
+        )}
       </div>
-      <input type={type}
-      name={name}
-      id={name}
-      placeholder={placeholder}
-      value={value}
-      onChange={handleChange}
-      required
-      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'/>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
+      />
     </div>
-  )
+  );
 }
