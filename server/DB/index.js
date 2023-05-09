837fce58d2d08b9cb35dd9c7645edec64d870465
DanielMalede <danielmalede@gmail.com>
2023-05-09 16:42:46 +0300

    routers

--- server/DB/index.js
+++ server/DB/index.js
@@ -1,1 +1,13 @@
 import mongoose from "mongoose";
+
+const connectDB = (url) => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
+};
+
+export default connectDB;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-
+import connectDB from "./DB/index.js";
+import postRouter from "./routes/postRouter.js";
+import dalleRouter from "./routes/dalleRouter.js";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+app.use("/posts", postRouter);
+app.use("/dalle", dalleRouter);
+
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
 });
 
 const startServer = async () => {
-  app.listen(process.env.PORT, () =>
-    console.log(`Server has started on port: ${process.env.PORT}`)
-  );
+  try {
+    connectDB(process.env.MONGO_URI);
+    app.listen(process.env.PORT, () =>
+      console.log(`Server has started on port: ${process.env.PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 startServer();
